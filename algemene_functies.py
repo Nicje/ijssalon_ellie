e24d0d0e46b259d16edd03a578fa21c5af246cb8
Nicole Bazen <koster065@hotmail.com>
2023-03-24 14:52:47 +0100

    functies toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,7 @@
+mijn_functie_1()
+invoer= "2", "4", "10", "12"
+print (invoer) * (invoer)
+
+mijn_functie_2()
+invoer= "12, 3", "12, 2", "10, 5", "100, 20"
+print (invoer) +-*/ (invoer)
---
+++ reclame.py
@@ -0,0 +1,31 @@
+from algemene_functies import mijn_functie_2
+
+aanbieding_1()
+smaak=aardbei
+prijs=4
+korting=0,1
+print (f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} voor {prijs-korting}.")
+
+
+mijn_lijst:(inkomsten_totaal)
+inkomsten= "220", "430", "125", "160", "205", "90", "345"
+print (inkomsten_totaal)
+bedrag=0,09% van (inkomsten_totaal)
+print (f"Het toaal van alle {inkomsten} van deze week is {inkomsten_totaal} euro, waarover {bedrag} euro BTW betaald dient te worden")
+
+mijn_lijst.append("min") ("max")
+print ("min, max inkomsten_totaal")
+
+mijn_lijst.append("gemiddelde")
+print ("gemiddelde" inkomsten-totaal)
+
+def meervoudig(invoer_lijst):
+    invoer_lijst = [10, 5, 3, 2, 1, 9]
+    tijdelijk = laag_en_hoog(invoer_lijst)
+    uitvoer = [tijdelijk[0],tijdelijk[1]]
+    return uitvoer
+
+def combinatie(invoer_lijst_2):
+    meervoudig(invoer_lijst_2)
+    mijn_functie_2=korte_lijst
+    
